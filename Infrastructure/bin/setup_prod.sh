63b7c3c7cf1e9370db5deff4557dd99c8ca677a3
ph-shah <prima_shah@rediffmail.com>
2018-08-20 14:26:12 +0100

    parksmap jenkins file

--- Infrastructure/bin/setup_prod.sh
+++ Infrastructure/bin/setup_prod.sh
@@ -83,4 +83,7 @@ echo ">>> STEP #3 > ADD VIEW PERMISSIONS"
 oc policy add-role-to-group view system:serviceaccount:${GUID}-parks-prod -n ${GUID}-parks-dev
 oc policy add-role-to-user view --serviceaccount=default -n $PROJECT_NAME
 
+oc policy add-role-to-user view --serviceaccount=default -n $GUID-parks-prod
+oc policy add-role-to-user edit system:serviceaccount:$GUID-jenkins:jenkins -n $GUID-parks-prod
+
 sleep 20
